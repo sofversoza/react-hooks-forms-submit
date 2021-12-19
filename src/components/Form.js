e8c66c14681c1e8ec8345764d45fd8cb4e9a0e1d
sofiaversoza <sofversoza@gmail.com>
2021-12-19 16:13:49 -0700

    commit tree 22f85e92cdf2f24502fe50f2f7f9d84253d48e5b

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,36 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      firstName: firstName,
+      lastName: lastName
+    };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");   // to clear the input fields-- were setting both states 
+    setLastName("");      // to an empty string once the data has been submitted
+  }
+
+  const listOfsubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleFirstNameChange} value={firstName} />
+          <input type="text" onChange={handleLastNameChange} value={lastName} />
+          <button type="submit">Submit</button>
+        </form>
+        <h3>Submissions</h3>
+        {listOfsubmissions}
+    </div>
   );
 }
 
